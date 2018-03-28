81ae61829171398603f10503366fecd83035b4a7
navrang-hotel <navrang.hotel.dev@gmail.com>
2018-03-28 13:07:18 +0545

    Installing base app

--- navrang/settings.py
+++ navrang/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Added by developer after this
+    'base.apps.BaseConfig',
 ]
 
 MIDDLEWARE = [
